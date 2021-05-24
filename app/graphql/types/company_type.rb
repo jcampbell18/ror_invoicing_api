61a45a32489b5a7ec858793a4b485e6e135866b7
Jason Campbell <jcryuu@gmail.com>
2021-05-23 21:13:11 -0700

    created full_address, but needs to be fixed

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -7,10 +7,16 @@ module Types
     field :city, String, null: true
     field :state, Types::StateType, null: false
     field :zipcode, String, null: true
+    field :full_address, String, null: false
     field :phone, String, null: true
     field :fax, String, null: true
     field :website_url, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+
+  def full_address
+    let city_state_zipcode([object.address, object.city, object.state.abbreviation].compact).join(', ')
+    ([city_state_zipcode, object.zipcode].compact).join(' ')
   end
 end
